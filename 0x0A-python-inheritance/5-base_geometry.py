63fc4df4ad974c1db4e6f6305f7f04b427ba088d
tundelakins <tundelakins4real@yahoo.com>
2022-07-04 21:00:35 +0000

    Python Inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+Contains empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass#!/usr/bin/python3
+"""
+Module 5-base_geometry
+Contains empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
