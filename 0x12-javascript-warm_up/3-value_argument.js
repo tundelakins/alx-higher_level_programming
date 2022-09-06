559c854f423d1461c257fce8abea6d54d681e9f1
tundelakins <tundelakins4real@yahoo.com>
2022-09-06 20:07:03 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const firstArg = process.argv[2];
-if (firstArg === undefined) {
-	  console.log('No argument');
+
+if (process.argv[2]) {
+  console.log(process.argv[2]);
 } else {
-	  console.log(firstArg);
+  console.log('No argument');
 }
