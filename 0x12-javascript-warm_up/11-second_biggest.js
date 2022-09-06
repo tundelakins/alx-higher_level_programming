4b675f8ae1e21fe4027da3e20aae93f1b4866188
tundelakins <tundelakins4real@yahoo.com>
2022-09-06 20:14:13 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,26 @@
 #!/usr/bin/node
-let args = process.argv.slice(2).map((x) => {
-  return parseInt(x);
-});
 
-if (args.length <= 1) {
-  console.log(0);
+const len = process.argv.length;
+const nums = process.argv.slice(2).map(function (n) {
+  return parseInt(n);
+});
+const max = Math.max.apply(Math, nums);
+const min = Math.min.apply(Math, nums);
+
+if (len > 3) {
+  let i = 0;
+  let n = 0;
+  let secBig = min;
+
+  for (; i < len; ++i) {
+    n = nums[i];
+
+    if (n > secBig && n < max) {
+      secBig = n;
+    }
+  }
+
+  console.log(secBig);
 } else {
-  console.log(args.sort((a, b) => {
-    return b - a;
-  })[1]);
+  console.log(0);
 }
