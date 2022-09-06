123f5b2f8e6f13dd2d3f0184010d5fdd454c689a
tundelakins <tundelakins4real@yahoo.com>
2022-09-06 19:46:00 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 t convertArg = parseInt(process.argv[2]);
 if (isNaN(convertArg)) {
-	  console.log('Not a number');
+  console.log('Not a number');
 } else {
-	  console.log('My number: ' + convertArg);
+  console.log('My number: ' + convertArg);
 }
