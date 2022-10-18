c692868285df239d56c1f76725f366755c130d71
tundelakins <tundelakins4real@yahoo.com>
2022-10-18 20:36:26 +0000

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,4 @@
 #!/usr/bin/node
+const fs = require('fs');
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
-  }
-});
+request('http://swapi.co/api/films/' + process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
